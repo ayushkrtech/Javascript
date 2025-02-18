04ec87cce30744ab72bf9a86cdea2743b44e5227
Ayush <techarunprd@gmail.com>
2025-02-18 13:21:45 +0530

    Add files via upload

---
+++ Control Flow/01_cf.js
@@ -0,0 +1,50 @@
+// if 
+
+// const isuserloggedin = true
+// const temperature =  12
+// if (temperature < 5){
+//     console.log("less than 15")
+// } else{
+//     console.log("temperature is greater than 50")
+// }
+// const score = 200
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);  
+// }
+// if (isuserloggedin){
+// }
+// if ( 3 === "3"){
+//     console.log("executed")    
+// }
+
+//          <,   >,   <=,   >=,    ==,    !=,   ===
+
+
+const balance = 1000
+// if (balance > 500) console.log("test"),
+// console.log("test1");
+
+// if (balance < 500){
+//     console.log("less than")
+// } else if (balance < 750) {
+//     console.log(" less than 750")
+// } else if (balance < 900){
+//     console.log("less than 900")
+// } else{
+//     console.log("less than 1200")
+// }
+
+
+const userloggedin = true 
+const debitcard = true
+const name = true
+const loggedfromgoogle = false
+const loggedfromemail = true
+
+// if (userloggedin && debitcard && name) {
+//     console.log("allow to buuy course")    
+// }
+// if (loggedfromgoogle || loggedfromemail){
+//     console.log("user logged in")
+// }
---
+++ Control Flow/04_truthy.js
@@ -0,0 +1,56 @@
+const useremail = []
+// if (useremail) {
+//     console.log("got the email");
+// } else {
+//     console.log("don't have user email");
+// }
+
+
+/* 
+
+falsy values
+false
+0 , -0 
+bigint 0n
+""
+null
+undefined
+NaN
+************************
+truthy values
+"0"
+'false'
+" "
+[]
+{}
+function(){}
+
+
+*/
+
+// if (useremail.length === 0) {
+//     console.log("array is empty");
+// }
+
+// const emptyobj = {}
+// if (Object.keys(emptyobj).length === 0) {
+//     console.log("obj is empty");
+    
+// }
+
+
+// NULLISH COALESCING OPERSTOR (??) : NULL METHOD
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? undefined ?? 12
+// console.log(val1);
+
+
+// terminary operator
+// condition ? true : false
+const iceteaprice = 100
+iceteaprice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
